2fed56d39468ba8ddafc007d8c6100cc9d421687
shikha192003 <99075623+shikha192003@users.noreply.github.com>
2022-02-05 23:04:28 +0530

    Add files via upload

---
+++ meraki12.py
@@ -0,0 +1,5 @@
+num=int(input("Please enter a no"))
+
+Dict = {x:x ** 2 for x in range(1, num + 1)}
+    
+print(Dict)
---
+++ meraki14.py
@@ -0,0 +1,3 @@
+dic={"bijender":45,"deepak":60,"param":20,"anjili":30,"roshini":50}
+sorted_dic_value = sorted(dic.values())
+print(sorted_dic_value)
